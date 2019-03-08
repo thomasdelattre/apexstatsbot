15cbef00483adada68a24e5411a88e0d7dfa23d9
thomasdelattre <thomas_delattre@hotmail.fr>
2019-03-08 10:59:09 +0100

    add express and serveur port

--- bot.js
+++ bot.js
@@ -1,8 +1,16 @@
 require('dotenv').load();
 var Discord = require('discord.io');
 var logger = require('winston');
-
-require('http').createServer().listen(3000);
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
