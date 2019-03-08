f401b669a2edaebdcded763e7ce28e3e1215c9ac
thomasdelattre <thomas_delattre@hotmail.fr>
2019-03-08 10:40:33 +0100

    add trycatch

--- bot.js
+++ bot.js
@@ -30,42 +30,39 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
         switch(cmd) {
             case 'apexstats':
-            var request = require('request'),
-                url = "https://public-api.tracker.gg/apex/v1/standard/profile/5/"+username;
-
-            request(
-                {
-                    url : url,
-                    headers : {
-                        "TRN-Api-Key" : "7488836b-98bc-46a9-859b-848fbb069057"
-                    }
-                },
-                function (error, response, body) {
-                    var content = JSON.parse(body);
-                    var message = '';
-                    if(content.errors){
-                        message = content.errors[0].message;
-                    }else{
-                        message = "**Joueur**: "+username+"\n**Level**: "+content.data.metadata.level;
-                        content.data.children.forEach(personnage => {
-                            message += "\n\n**Champion**: "+personnage.metadata.legend_name;
-                            personnage.stats.forEach(stat => {
-                                if(username == "Gadhena"){ stat.displayValue *= 56;}
-                                message +="\n**"+stat.metadata.name+"**: "+stat.displayValue
-                                
+            var request = require('request'), url = "https://public-api.tracker.gg/apex/v1/standard/profile/5/"+username;
+            
+            try {
+                request(
+                    {
+                        url : url,
+                        headers : {
+                            "TRN-Api-Key" : "7488836b-98bc-46a9-859b-848fbb069057"
+                        }
+                    },
+                    function (error, response, body) {
+                        var content = JSON.parse(body);
+                        var message = '';
+                        if(content.errors){
+                            message = content.errors[0].message;
+                        }else{
+                            message = "**Joueur**: "+username+"\n**Level**: "+content.data.metadata.level;
+                            content.data.children.forEach(personnage => {
+                                message += "\n\n**Champion**: "+personnage.metadata.legend_name;
+                                personnage.stats.forEach(stat => {
+                                    message +="\n**"+stat.metadata.name+"**: "+stat.displayValue
+                                });
                             });
-                        });
+                        }
+                        bot.sendMessage({
+                            to: channelID,
+                            message: message
+                        });                
                     }
-                    if(username == "Elthari14"){
-                        message += "\n\nT'es nul, aide nous un peu"
-                    }
-                    bot.sendMessage({
-                        to: channelID,
-                        message: message
-                    });
-                
-                }
-            );             
+                );       
+            } catch (error) {
+                bot.sendMessage({to: channelID, message: error});
+            }      
             break;
          }
      }
